4878a41a0a7be5392c4b0177a64e6bc757938d7c
Gavin Schmidt <gavinschmidt47@yahoo.com>
2025-09-21 23:06:59 -0400

    Turn manager setup

--- Reconnection/Intermediate/Build/Win64/UnrealEditor/Inc/Reconnection/UHT/Fighter.generated.h
+++ Reconnection/Intermediate/Build/Win64/UnrealEditor/Inc/Reconnection/UHT/Fighter.generated.h
@@ -17,14 +17,14 @@
 PRAGMA_DISABLE_DEPRECATION_WARNINGS
 
 // ********** Begin Class AFighter *****************************************************************
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
 	DECLARE_FUNCTION(execEndTurn); \
 	DECLARE_FUNCTION(execStartTurn);
 
 
 RECONNECTION_API UClass* Z_Construct_UClass_AFighter_NoRegister();
 
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_INCLASS_NO_PURE_DECLS \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesAFighter(); \
 	friend struct Z_Construct_UClass_AFighter_Statics; \
@@ -35,7 +35,7 @@ public: \
 	DECLARE_SERIALIZER(AFighter)
 
 
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_ENHANCED_CONSTRUCTORS \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_ENHANCED_CONSTRUCTORS \
 	/** Deleted move- and copy-constructors, should never be used */ \
 	AFighter(AFighter&&) = delete; \
 	AFighter(const AFighter&) = delete; \
@@ -45,13 +45,13 @@ public: \
 	NO_API virtual ~AFighter();
 
 
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_9_PROLOG
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_GENERATED_BODY \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_9_PROLOG
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
-	FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_INCLASS_NO_PURE_DECLS \
-	FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_ENHANCED_CONSTRUCTORS \
+	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h_12_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -61,6 +61,6 @@ class AFighter;
 // ********** End Class AFighter *******************************************************************
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h
+#define CURRENT_FILE_ID FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_Fighter_h
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Reconnection/Intermediate/Build/Win64/x64/ReconnectionEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.InclOrderUnreal5_3.cpp
+++ Reconnection/Intermediate/Build/Win64/x64/ReconnectionEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.InclOrderUnreal5_3.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.38.33145
+// Compiler: 14.43.34810
 #include "SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.InclOrderUnreal5_3.h"
