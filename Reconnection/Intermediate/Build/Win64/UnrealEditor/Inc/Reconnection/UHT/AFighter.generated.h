7648088165c8de92874a5caca7a2dbd56f8445a9
Wesley Yates <woyates04@gmail.com>
2025-09-28 19:24:40 -0400

    Combat Widgets Added

--- Reconnection/Intermediate/Build/Win64/UnrealEditor/Inc/Reconnection/UHT/AFighter.generated.h
+++ Reconnection/Intermediate/Build/Win64/UnrealEditor/Inc/Reconnection/UHT/AFighter.generated.h
@@ -19,7 +19,7 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 class AFighter;
 
 // ********** Begin Class AFighter *****************************************************************
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
 	DECLARE_FUNCTION(execAttack); \
 	DECLARE_FUNCTION(execBlock); \
 	DECLARE_FUNCTION(execRemoveBuff); \
@@ -37,10 +37,10 @@ class AFighter;
 	DECLARE_FUNCTION(execOnHitAttack);
 
 
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_CALLBACK_WRAPPERS
+#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_CALLBACK_WRAPPERS
 RECONNECTION_API UClass* Z_Construct_UClass_AFighter_NoRegister();
 
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_INCLASS_NO_PURE_DECLS \
+#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesAFighter(); \
 	friend struct Z_Construct_UClass_AFighter_Statics; \
@@ -51,7 +51,7 @@ public: \
 	DECLARE_SERIALIZER(AFighter)
 
 
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_ENHANCED_CONSTRUCTORS \
+#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_ENHANCED_CONSTRUCTORS \
 	/** Deleted move- and copy-constructors, should never be used */ \
 	AFighter(AFighter&&) = delete; \
 	AFighter(const AFighter&) = delete; \
@@ -61,14 +61,14 @@ public: \
 	NO_API virtual ~AFighter();
 
 
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_9_PROLOG
-#define FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_GENERATED_BODY \
+#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_9_PROLOG
+#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
-	FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_CALLBACK_WRAPPERS \
-	FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_INCLASS_NO_PURE_DECLS \
-	FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_ENHANCED_CONSTRUCTORS \
+	FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_CALLBACK_WRAPPERS \
+	FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h_12_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -78,6 +78,6 @@ class AFighter;
 // ********** End Class AFighter *******************************************************************
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID FID_Users_gavin_OneDrive_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h
+#define CURRENT_FILE_ID FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_AFighter_h
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
