9fe37c9d39bb889dcefb04485de2dc47b3139644
ddiaz24 <108260919+ddiaz24@users.noreply.github.com>
2025-10-08 08:59:01 -0400

    Static Camera Added

--- Reconnection/Intermediate/Build/Win64/UnrealEditor/Inc/Reconnection/UHT/AEnemy.generated.h
+++ Reconnection/Intermediate/Build/Win64/UnrealEditor/Inc/Reconnection/UHT/AEnemy.generated.h
@@ -17,14 +17,14 @@
 PRAGMA_DISABLE_DEPRECATION_WARNINGS
 
 // ********** Begin Class AEnemy *******************************************************************
-#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+#define FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
 	DECLARE_FUNCTION(execChooseAction);
 
 
-#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_CALLBACK_WRAPPERS
+#define FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_CALLBACK_WRAPPERS
 RECONNECTION_API UClass* Z_Construct_UClass_AEnemy_NoRegister();
 
-#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_INCLASS_NO_PURE_DECLS \
+#define FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesAEnemy(); \
 	friend struct Z_Construct_UClass_AEnemy_Statics; \
@@ -35,7 +35,7 @@ public: \
 	DECLARE_SERIALIZER(AEnemy)
 
 
-#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_ENHANCED_CONSTRUCTORS \
+#define FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_ENHANCED_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API AEnemy(); \
 	/** Deleted move- and copy-constructors, should never be used */ \
@@ -47,14 +47,14 @@ public: \
 	NO_API virtual ~AEnemy();
 
 
-#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_12_PROLOG
-#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_GENERATED_BODY \
+#define FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_12_PROLOG
+#define FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
-	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_CALLBACK_WRAPPERS \
-	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_INCLASS_NO_PURE_DECLS \
-	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_ENHANCED_CONSTRUCTORS \
+	FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_CALLBACK_WRAPPERS \
+	FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h_15_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -64,6 +64,6 @@ class AEnemy;
 // ********** End Class AEnemy *********************************************************************
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h
+#define CURRENT_FILE_ID FID_Users_Da944322_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_AEnemy_h
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
