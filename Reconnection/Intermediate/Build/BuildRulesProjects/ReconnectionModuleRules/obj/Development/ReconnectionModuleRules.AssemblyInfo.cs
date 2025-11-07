734e5f30829f42c06bb94ae0fec376d5aacd306c
Gavin Schmidt <51172158+gavinschmidt47@users.noreply.github.com>
2025-11-07 12:40:44 -0500

    Added dialogue collector

--- Reconnection/Intermediate/Build/BuildRulesProjects/ReconnectionModuleRules/obj/Development/ReconnectionModuleRules.AssemblyInfo.cs
+++ Reconnection/Intermediate/Build/BuildRulesProjects/ReconnectionModuleRules/obj/Development/ReconnectionModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0257c43328d2e99eaba569659a46033da144b93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a109f23a37d97511a6bc36fe31609e10017d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
