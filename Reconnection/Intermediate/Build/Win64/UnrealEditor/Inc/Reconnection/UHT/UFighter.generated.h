efc26e952be8c6011bc068131d30d8924f9deb18
Gavin Schmidt <51172158+gavinschmidt47@users.noreply.github.com>
2025-12-01 10:40:43 -0500

    Combat fix and audio setting added

--- Reconnection/Intermediate/Build/Win64/UnrealEditor/Inc/Reconnection/UHT/UFighter.generated.h
+++ Reconnection/Intermediate/Build/Win64/UnrealEditor/Inc/Reconnection/UHT/UFighter.generated.h
@@ -19,35 +19,35 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 class UFighter;
 
 // ********** Begin Delegate FOnStartTurn **********************************************************
-#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_9_DELEGATE \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_9_DELEGATE \
 RECONNECTION_API void FOnStartTurn_DelegateWrapper(const FMulticastScriptDelegate& OnStartTurn, UFighter* Fighter);
 
 
 // ********** End Delegate FOnStartTurn ************************************************************
 
 // ********** Begin Delegate FOnHitAttack **********************************************************
-#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_10_DELEGATE \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_10_DELEGATE \
 RECONNECTION_API void FOnHitAttack_DelegateWrapper(const FMulticastScriptDelegate& OnHitAttack, UFighter* Fighter);
 
 
 // ********** End Delegate FOnHitAttack ************************************************************
 
 // ********** Begin Delegate FOnHitMiss ************************************************************
-#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_11_DELEGATE \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_11_DELEGATE \
 RECONNECTION_API void FOnHitMiss_DelegateWrapper(const FMulticastScriptDelegate& OnHitMiss, UFighter* Fighter);
 
 
 // ********** End Delegate FOnHitMiss **************************************************************
 
 // ********** Begin Delegate FOnEndTurn ************************************************************
-#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_12_DELEGATE \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_12_DELEGATE \
 RECONNECTION_API void FOnEndTurn_DelegateWrapper(const FMulticastScriptDelegate& OnEndTurn, UFighter* Fighter);
 
 
 // ********** End Delegate FOnEndTurn **************************************************************
 
 // ********** Begin Class UFighter *****************************************************************
-#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_RPC_WRAPPERS_NO_PURE_DECLS \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_RPC_WRAPPERS_NO_PURE_DECLS \
 	DECLARE_FUNCTION(execGetAllStats); \
 	DECLARE_FUNCTION(execDie); \
 	DECLARE_FUNCTION(execAttack); \
@@ -66,10 +66,10 @@ RECONNECTION_API void FOnEndTurn_DelegateWrapper(const FMulticastScriptDelegate&
 	DECLARE_FUNCTION(execStartTurn);
 
 
-#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_CALLBACK_WRAPPERS
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_CALLBACK_WRAPPERS
 RECONNECTION_API UClass* Z_Construct_UClass_UFighter_NoRegister();
 
-#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_INCLASS_NO_PURE_DECLS \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesUFighter(); \
 	friend struct Z_Construct_UClass_UFighter_Statics; \
@@ -80,7 +80,7 @@ public: \
 	DECLARE_SERIALIZER(UFighter)
 
 
-#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_ENHANCED_CONSTRUCTORS \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_ENHANCED_CONSTRUCTORS \
 	/** Deleted move- and copy-constructors, should never be used */ \
 	UFighter(UFighter&&) = delete; \
 	UFighter(const UFighter&) = delete; \
@@ -90,14 +90,14 @@ public: \
 	NO_API virtual ~UFighter();
 
 
-#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_15_PROLOG
-#define FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_GENERATED_BODY \
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_15_PROLOG
+#define FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_RPC_WRAPPERS_NO_PURE_DECLS \
-	FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_CALLBACK_WRAPPERS \
-	FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_INCLASS_NO_PURE_DECLS \
-	FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_ENHANCED_CONSTRUCTORS \
+	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_CALLBACK_WRAPPERS \
+	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_INCLASS_NO_PURE_DECLS \
+	FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h_18_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
@@ -107,6 +107,6 @@ class UFighter;
 // ********** End Class UFighter *******************************************************************
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID FID_Users_woyat_OneDrive_Projects_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h
+#define CURRENT_FILE_ID FID_Users_gavin_Documents_GitHub_Reconnection_Reconnection_Source_Reconnection_Public_UFighter_h
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
