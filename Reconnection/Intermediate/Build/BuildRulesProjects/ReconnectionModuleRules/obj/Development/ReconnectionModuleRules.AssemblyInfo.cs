8fc5947c744e54413b15d0429eda2c0d998b8a66
Wesley Yates <woyates04@gmail.com>
2026-01-16 01:09:04 -0500

    Object for grid made
    Vector variable added, Might experiment tomorrow

--- Reconnection/Intermediate/Build/BuildRulesProjects/ReconnectionModuleRules/obj/Development/ReconnectionModuleRules.AssemblyInfo.cs
+++ Reconnection/Intermediate/Build/BuildRulesProjects/ReconnectionModuleRules/obj/Development/ReconnectionModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f2a22adf25d6cfbbd1e6a8d67d9d230e48d87b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a430b26d15e901024086e45031f88cbb5dab1076")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
