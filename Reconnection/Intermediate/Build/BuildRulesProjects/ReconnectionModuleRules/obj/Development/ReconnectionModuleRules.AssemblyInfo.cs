1482b9aa43f3e9e80678bae8d565e58fd78a955c
FearlessMP <Mpolfer27@gmail.com>
2026-01-28 17:14:43 -0500

    Inserted Level 1 Assets (Partially Done)
    Placed down assets for the forest outside of the fort and placed some smaller assets inside the fort. Will continue after dinner break.

--- Reconnection/Intermediate/Build/BuildRulesProjects/ReconnectionModuleRules/obj/Development/ReconnectionModuleRules.AssemblyInfo.cs
+++ Reconnection/Intermediate/Build/BuildRulesProjects/ReconnectionModuleRules/obj/Development/ReconnectionModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f94b61223aa6e3157c3c3a990f6f350321d811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9cb6a02e60ac3d44f582d1e748b6438c54f00a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
