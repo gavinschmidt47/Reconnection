1b8cbf0f9c90a5bd2f29aa2fbef7ed497b2dc4d2
FearlessMP <122576116+FearlessMP@users.noreply.github.com>
2026-02-02 09:56:58 -0500

    Enemies Updated in Size and Texture
    Enemies have been resized to the team's liking and proper textures have been applied to all enemies

--- Reconnection/Intermediate/Build/BuildRulesProjects/ReconnectionModuleRules/obj/Development/ReconnectionModuleRules.AssemblyInfo.cs
+++ Reconnection/Intermediate/Build/BuildRulesProjects/ReconnectionModuleRules/obj/Development/ReconnectionModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f22f25f577c440286799d0a1b9df7a2db287a13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+005462aba005c7a7002290a5bfa015812b76b416")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReconnectionModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
